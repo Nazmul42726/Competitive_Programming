c9a3856f7510f3cf480484ac3a480199139ef3f1
Nazmul Hasan <nazmulhasancsecu@gmail.com>
2024-02-24 17:26:48 +0600

    another solution added

---
+++ Algebra/Euclidean_GCD/Greatest_Common_Divisor.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+signed main(){
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    optimize();
+    int x,y;
+    cin>>x>>y;
+    cout<<__gcd(x,y)<<endl;         //another useless problem
+    return 0;
+}
