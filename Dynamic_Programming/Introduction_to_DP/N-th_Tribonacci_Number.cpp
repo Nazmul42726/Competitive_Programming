210ab7bf03a012d9926f805ff9e7744c87f9adbe
Nazmul Hasan <nazmulhasancsecu@gmail.com>
2024-02-25 00:58:12 +0600

    1st DP problem (easy)

---
+++ Dynamic_Programming/Introduction_to_DP/N-th_Tribonacci_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arr[4];
+    int tribonacci(int n) {
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=1;
+        for(int i=3; i<=n; i++){
+            arr[i%4]=arr[(i-1)%4]+arr[(i-2)%4]+arr[(i-3)%4];
+        }
+        return arr[n%4];
+    }
+};
