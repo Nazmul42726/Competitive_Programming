f4e41c81d761c059b5b3f3255a836bebd9d3f15f
Nazmul Hasan <nazmulhasancsecu@gmail.com>
2024-03-06 21:18:26 +0600

    another implementation

--- Algebra/Sieve_of_Eratosthenes/prime_In_Range.cpp
+++ Algebra/Sieve_of_Eratosthenes/prime_In_Range.cpp
@@ -4,7 +4,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<char> segmentedSieve(long long l, long long r){
+vector<char> segmentedSieve(long long l, long long r){      //pregenerating primes [1, sqrt(r)] // more efficient
     long long preGenerateLimit=sqrt(r);
     vector<char> primeSieve(preGenerateLimit+1,true);
     vector<long long> preGeneratedPrimes;
@@ -24,10 +24,20 @@ vector<char> segmentedSieve(long long l, long long r){
     if(l==1) result[0]=false;       //surprise!
     return result;
 }
+vector<char> segmented_Sieve(long long l, long long r){     //without pregenerating primes [1, sqrt(r)]
+    vector<char> result(r-l+1,true);
+    int limit=sqrt(r);
+    for(long long i=2; i<=limit; i++){
+        for(long long j=max(i*i,(long long)ceil(l*1.0/i)*i); j<=r; j+=i)
+            result[j-l]=false;
+    }
+    if(l==1) result[0]=false;
+    return result;
+}
 int main(){
     long long l,r;
     cin>>l>>r;
-    vector<char> sieve=segmentedSieve(l,r);
+    vector<char> sieve=segmented_Sieve(l,r);
     for(int i=0; i<sieve.size(); i++){
         if(sieve[i]) cout<<i+l<<" ";
     }
