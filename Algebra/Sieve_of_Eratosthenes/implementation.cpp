30059f5668166b0ac2930253b9d01e8865663414
Nazmul Hasan <nazmulhasancsecu@gmail.com>
2024-03-06 14:32:31 +0600

    generate prime sieve

---
+++ Algebra/Sieve_of_Eratosthenes/implementation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<bool> primeSieve(n+1,true);
+    primeSieve[0]=primeSieve[1]=false;
+    for(int i=2; i*i<n; i++){
+        if(primeSieve[i])
+            for(int j=i*i; j<=n; j+=i) primeSieve[j]=false;
+    }
+
+    cout<<"Prime numbers [1,"<<n<<"]: "<<endl;
+    for(int i=2; i<=n; i++){
+        if(primeSieve[i]) cout<<i<<" ";
+    }
+    cout<<endl;
+}
